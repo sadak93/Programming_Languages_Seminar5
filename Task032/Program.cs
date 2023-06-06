8d565879985a05db04b026e53459508ec07ada6a
Sadak <s.sadak02@gmail.com>
2023-06-06 18:05:20 +0600

    Замена чисел массива на противоположенный

---
+++ Task032/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 32: Напишите программу замена элементов массива: 
+// положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-9, 10);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    foreach (var item in array)
+    {
+        Console.Write($"{item} ");
+    }
+    System.Console.WriteLine();
+}
+
+void Zamena(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i]=-1 * array[i];
+        Console.Write($"{array[i]} ");
+    }
+    
+}
+
+Console.Clear();
+System.Console.WriteLine("Введите размерность массива ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+FillArray(array);
+PrintArray(array);
+Zamena (array);
