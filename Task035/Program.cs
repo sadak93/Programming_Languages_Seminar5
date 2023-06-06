0a9404530715a09e6df10e6c03c9dfe644b4f64e
Sadak <s.sadak02@gmail.com>
2023-06-06 18:06:57 +0600

    Поиск промежутка значений в массиве

---
+++ Task035/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 35: Задайте одномерный массив из 15 случайных чисел от -100 до 100. 
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,99].  
+// Пример для массива из 5, а не 15 элементов. В своём решении сделайте для 15 
+// [5, 18, 123, 6, 2] -> 1 
+// [1, 2, 3, 6, 2] -> 0 
+// [10, 11, 12, 13, 14] -> 5
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-100, 101);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    foreach (var item in array)
+    {
+        Console.Write($"{item} ");
+    }
+    System.Console.WriteLine();
+}
+
+int SearchArray (int [] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]>9 && array[i]<100)
+            count ++;
+    }
+    return count;
+}
+
+Console.Clear();
+System.Console.WriteLine("Введите размерность массива ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+
+FillArray(array);
+PrintArray(array);
+System.Console.WriteLine(SearchArray(array)); 
